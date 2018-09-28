8eb97b2c85c5a9d1070cc477dfcffbd13f5065fe
jelambrar96 <jelambrar@gmail.com>
2018-09-28 10:55:10 -0500

    first commit

---
+++ init.sh
@@ -0,0 +1,6 @@
+echo "# color_processing" >> README.md
+git init
+git add *
+git commit -m "first commit"
+git remote add origin git@github.com:jelambrar96/color_processing.git
+git push -u origin master
